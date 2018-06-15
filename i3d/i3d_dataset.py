f4bfaa5a1c7388ec613268bf7bf824b58ca8757e
Anton P. Linevich <anton@linevich.com>
2018-06-15 17:25:23 +0000

    generate all_seq once

--- i3d/i3d_dataset.py
+++ i3d/i3d_dataset.py
@@ -73,6 +73,100 @@ def load_activity_net_positives(data_dir):
             cap.release()
 
 
+def load_sequences(data_dir, num_frames_in_sequence, train_txt):
+    all_seq = []
+    cls1_count = 0
+    cls0_count = 0
+    if train_txt == "train.txt":
+        print("Loading activity_net", train_txt)
+        # Load activity-net positives {{{
+        with open(data_dir + '/activity_net-positives.csv') as _f:
+            rdr = csv.reader(_f)
+
+            cap = cv2.VideoCapture()
+            for row in rdr:
+                _, id, start_sec, end_sec, _ = row
+
+                video_fn = "activity_net/%s.mp4" % id
+                # cap = cv2.VideoCapture(data_dir + "/" + video_fn)
+                cap.open(data_dir + "/" + video_fn)
+                fps = cap.get(cv2.CAP_PROP_FPS)
+                total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
+                cap.release()
+
+                _start_fn = int(fps * float(start_sec))
+                _end_fn = int(fps * float(end_sec))
+                # print(id, _start_fn, _end_fn)
+
+                for moment_start in range(_start_fn,
+                                          min(_end_fn + num_frames_in_sequence + 4, total_frames),
+                                          num_frames_in_sequence):
+                    start_fn = moment_start
+                    end_fn = start_fn + num_frames_in_sequence
+
+                    # train sequence
+                    cls_seq = []
+                    for _fn in range(start_fn, end_fn):
+                        cls_seq.append((_fn, [0, 1.0]))
+                    all_seq.append((1, video_fn, cls_seq))
+                    cls1_count = cls1_count + 1
+        # }}}
+    with open(os.path.join(data_dir, train_txt), 'r') as video_fn:
+        train_files = list(map(lambda l: l.strip(), video_fn.readlines()))
+    for video_fn in train_files:
+        if video_fn.startswith("no_smoking_videos"):
+            # Folder for negatives
+
+            # Choose random clip from the video
+            cap = cv2.VideoCapture(os.path.join(data_dir, video_fn))
+            total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
+            cap.release()
+
+            for moment_start in range(0, int(total_frames / num_frames_in_sequence) - 1):
+                start_fn = moment_start
+                end_fn = start_fn + num_frames_in_sequence
+
+                # train sequence
+                cls_seq = []
+                for _fn in range(start_fn, end_fn):
+                    cls_seq.append((_fn, [1.0, 0]))
+                all_seq.append((0, video_fn, cls_seq))
+                cls0_count = cls0_count + 1
+            continue
+
+        human_y = os.path.join(data_dir, "jsonl.byhuman", video_fn, 'result.jsonl')
+        # machine_y = os.path.join(self.data_dir, "jsonl", _f, 'result.jsonl')
+
+        with open(human_y) as f:
+            predictions_by_frame = [json.loads(s.strip()) for s in f]
+
+            p_prev = None
+            cls_seq = []
+            for fn_p in predictions_by_frame:
+                fn, p = fn_p
+                cls_id = int(argmax(p))
+                if p_prev is None:
+                    p_prev = p
+
+                if p_prev[0] == p[0]:
+                    cls_seq.append(fn_p)
+
+                if len(cls_seq) == num_frames_in_sequence:
+                    all_seq.append((cls_id, video_fn, cls_seq))
+
+                    # Stats
+                    if cls_id == 1:
+                        cls1_count = cls1_count + 1
+                    if cls_id == 0:
+                        cls0_count = cls0_count + 1
+
+                    cls_seq = []
+                p_prev = p
+
+    print("Class distribution: cls1=%d; cls0=%d" % (cls1_count, cls0_count))
+    return all_seq
+
+
 class I3DFusionSequence(Sequence):
 
     def __init__(self, data_dir, train_txt: str, batch_size: int = 32, input_hw=(224, 224), show=False,
@@ -87,100 +181,20 @@ class I3DFusionSequence(Sequence):
         self.image_augmentation = None
         # self.image_augmentation = augmentation.ImageAugmentation()
 
-        cls1_count = 0
-        cls0_count = 0
+        self.cap = cv2.VideoCapture()
 
-        if train_txt == "train.txt":
-            print("Loading activity_net", train_txt)
-            # Load activity-net positives {{{
-            with open(data_dir + '/activity_net-positives.csv') as _f:
-                rdr = csv.reader(_f)
-
-                cap = cv2.VideoCapture()
-                for row in rdr:
-                    _, id, start_sec, end_sec, _ = row
-
-                    video_fn = "activity_net/%s.mp4" % id
-                    # cap = cv2.VideoCapture(data_dir + "/" + video_fn)
-                    cap.open(data_dir + "/" + video_fn)
-                    fps = cap.get(cv2.CAP_PROP_FPS)
-                    total_frames = int(cap.get(cv2.CAP_PROP_FRAME_COUNT))
-                    cap.release()
-
-                    _start_fn = int(fps * float(start_sec))
-                    _end_fn = int(fps * float(end_sec))
-                    # print(id, _start_fn, _end_fn)
-
-                    for moment_start in range(_start_fn,
-                                              min(_end_fn + num_frames_in_sequence + 4, total_frames),
-                                              num_frames_in_sequence):
-                        start_fn = moment_start
-                        end_fn = start_fn + num_frames_in_sequence
-
-                        # train sequence
-                        cls_seq = []
-                        for _fn in range(start_fn, end_fn):
-                            cls_seq.append((_fn, [0, 1.0]))
-                        self.all_seq.append((1, video_fn, cls_seq))
-                        cls1_count = cls1_count + 1
-            # }}}
-
-        with open(os.path.join(data_dir, train_txt), 'r') as video_fn:
-            train_files = list(map(lambda l: l.strip(), video_fn.readlines()))
-
-        for video_fn in train_files:
-            if video_fn.startswith("no_smoking_videos"):
-                # Folder for negatives
-
-                # Choose random clip from the video
-                cap = cv2.VideoCapture(os.path.join(self.data_dir, video_fn))
-                total_frames = cap.get(cv2.CAP_PROP_FRAME_COUNT)
-                cap.release()
-
-                for moment_start in range(0, int(total_frames / num_frames_in_sequence) - 1):
-                    start_fn = moment_start
-                    end_fn = start_fn + num_frames_in_sequence
-
-                    # train sequence
-                    cls_seq = []
-                    for _fn in range(start_fn, end_fn):
-                        cls_seq.append((_fn, [1.0, 0]))
-                    self.all_seq.append((0, video_fn, cls_seq))
-                    cls0_count = cls0_count + 1
-                continue
-
-            human_y = os.path.join(self.data_dir, "jsonl.byhuman", video_fn, 'result.jsonl')
-            # machine_y = os.path.join(self.data_dir, "jsonl", _f, 'result.jsonl')
-
-            with open(human_y) as f:
-                predictions_by_frame = [json.loads(s.strip()) for s in f]
-
-                p_prev = None
-                cls_seq = []
-                for fn_p in predictions_by_frame:
-                    fn, p = fn_p
-                    cls_id = argmax(p)
-                    if p_prev is None:
-                        p_prev = p
-
-                    if p_prev[0] == p[0]:
-                        cls_seq.append(fn_p)
-
-                    if len(cls_seq) == self.num_frames:
-                        self.all_seq.append((cls_id, video_fn, cls_seq))
-
-                        # Stats
-                        if cls_id == 1:
-                            cls1_count = cls1_count + 1
-                        if cls_id == 0:
-                            cls0_count = cls0_count + 1
-
-                        cls_seq = []
-                    p_prev = p
+        if os.path.isfile("all_seq.json"):
+            with open('all_seq.json', 'r') as _f:
+                self.all_seq = json.load(_f)
+        else:
+            self.all_seq = load_sequences(data_dir, num_frames_in_sequence, train_txt)
+            with open('all_seq.json', 'w') as _f:
+                json.dump(self.all_seq, _f)
 
         assert len(self.all_seq) > 0, "empty dataset. something is wrong"
+
+        # kinda this: all_seq = [  (1, video_fn, cls_seq), ..., ...  ]
         shuffle(self.all_seq)
-        print("Class distribution: cls1=%d; cls0=%d" % (cls1_count, cls0_count))
 
     def __getitem__(self, index):
         s = index * self.batch_size
@@ -189,10 +203,10 @@ class I3DFusionSequence(Sequence):
         xrgb_batch = numpy.zeros(shape=(self.batch_size, self.num_frames, self.input_hw[0], self.input_hw[1], 3))
         xflow_batch = numpy.zeros(shape=(self.batch_size, self.num_frames, self.input_hw[0], self.input_hw[1], 2))
         ybatch = numpy.zeros(shape=(self.batch_size, 2))
-        cap = cv2.VideoCapture()
+
         for ii in range(s, e):
             i = ii - s
-            xrgb, xflow, y = self.get_one_xy(ii, cap=cap)
+            xrgb, xflow, y = self.get_one_xy(ii, cap=self.cap)
             xrgb_batch[i, :, :, :, :] = xrgb
             xflow_batch[i, :, :, :, :] = xflow
             ybatch[i, :] = y
